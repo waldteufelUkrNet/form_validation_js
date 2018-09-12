68a0d0c99d8946315a2a09d53b6709471442cf75
waldteufel <waldteufel@ukr.net>
2018-09-12 17:32:30 +0300

    officework 2018.09.12

--- app/js-expanded/formValidationJs.js
+++ app/js-expanded/formValidationJs.js
@@ -1,22 +1,20 @@
 // made by waldteufel@ukr.net
 
-// .forvalJs-notEmpty
-
+// .forvalJs-notEmpty + data-forvalJs-notEmpty='number'
+// .forvalJs-email                                                           : x@x.x
+// .forvalJs-price                                                           : number.number / number
+// .forvalJs-password (.forvalJs-notEmpty + data-forvalJs-notEmpty='number') : number + symbol (length)
+// .forvalJs-confirmPassword                                                 : password = password
 
 $(document).ready(function(){
 
   $('.forvalJs-notEmpty').blur(function(){
-    if ($(this).val().length < 5) {
-      if ($(this).next().next('.forvalJs-prompting')[0]) return;
-      drawPromptingElem(this, 'The length of the field is at least 5 symbols');
-    }
+    validateNotEmpty (this);
   });
 
   $('.forvalJs-notEmpty').keyup(function(){
-    if ($(this).val().length >= 5) {
-      if ($(this).next().next('.forvalJs-prompting')[0]) {
-        removePromptingElem($(this).next().next('.forvalJs-prompting')[0]);
-      }
+    if ($(this).next().next('.forvalJs-prompting')[0]) {
+      validateNotEmpty (this);
     }
   });
 
@@ -25,16 +23,75 @@ $(document).ready(function(){
   });
 
   $('.forvalJs-email').keyup(function(){
-    //validateEmail(this);
+    if ($(this).next().next('.forvalJs-prompting')[0]) {
+      validateEmail (this);
+    }
+  });
+
+  $('.forvalJs-price').blur(function(){
+    validatePrice(this)
+  });
+
+  $('.forvalJs-price').keyup(function(){
+    if ($(this).next().next('.forvalJs-prompting')[0]) {
+      validatePrice (this);
+    }
+  });
+
+  $('.forvalJs-password').blur(function(){
+    validatePassword(this)
+  });
+
+  $('.forvalJs-password').keyup(function(){
+    if ($(this).next().next('.forvalJs-prompting')[0]) {
+      validatePassword (this);
+    }
+  });
+
+  $('.forvalJs-confirmPassword').blur(function(){
+    validateConfirmPassword(this)
+  });
+
+  $('.forvalJs-confirmPassword').keyup(function(){
+    if ($(this).next().next('.forvalJs-prompting')[0]) {
+      validateConfirmPassword (this);
+    }
   });
 
 });
 
+function validateNotEmpty (elem) {
+  var tempDataVal = +$(elem).attr('data-forvalJs-notEmpty') || 0;
+  var tempVal = $(elem).val();
+
+  if (tempDataVal == 0 && tempVal == '' ) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('this field can not be empty');
+      return
+    }
+    drawPromptingElem(elem, 'this field can not be empty');
+    return
+  }
+  if (tempDataVal != 0 && $(elem).val().length < tempDataVal) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('The length of the field is at least ' + tempDataVal + ' symbols');
+      return
+    }
+    drawPromptingElem(elem, 'The length of the field is at least ' + tempDataVal + ' symbols');
+    return
+  }
+  removePromptingElem($(elem).next().next('.forvalJs-prompting')[0]);
+}
+
 function validateEmail(elem) {
   var tempVal = $(elem).val();
+
+  if (tempVal.length == 0) return
+
   var charAmount = calculateCharsInStr(tempVal, '@');
   if (charAmount != 1) {
     if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('must be a @-character (but only one)');
       return
     }
     drawPromptingElem(elem, 'must be a @-character (but only one)');
@@ -43,14 +100,16 @@ function validateEmail(elem) {
   var tempArr = tempVal.split('@');
   if(tempArr[0].length < 1 || tempArr[1].length < 1) {
     if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('mail can not begin or end with a @-character');
       return
     }
-    drawPromptingElem(elem, 'mail can not begin and end with a @-character');
+    drawPromptingElem(elem, 'mail can not begin or end with a @-character');
     return
   }
   charAmount = calculateCharsInStr(tempArr[1], '.');
   if (charAmount != 1) {
     if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('incorrect domain name');
       return
     }
     drawPromptingElem(elem, 'incorrect domain name');
@@ -59,12 +118,108 @@ function validateEmail(elem) {
   tempArr = tempArr[1].split('.');
   if(tempArr[0].length < 1 || tempArr[1].length < 1) {
     if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('incorrect domain name');
       return
     }
     drawPromptingElem(elem, 'incorrect domain name');
     return
   }
+  removePromptingElem ($(elem).next().next('.forvalJs-prompting')[0])
+}
 
+function validatePrice(elem) {
+  var tempVal = $(elem).val();
+
+  if (tempVal.length == 0) return
+
+  if (calculateCharsInStr(tempVal, ' ') != 0) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('the price should be a number (can only contain numbers and a dot)');
+      return
+    }
+    drawPromptingElem(elem, 'the price should be a number (can only contain numbers and a dot)');
+    return
+  }
+
+  var charAmount = calculateCharsInStr(tempVal, '.');
+
+  if (charAmount < 1) {
+    if(!isNumeric(tempVal)) {
+      if ($(elem).next().next('.forvalJs-prompting')[0]) {
+        $($(elem).next().next('.forvalJs-prompting')[0]).text('the price should be a number (can only contain numbers and a dot)');
+        return
+      }
+      drawPromptingElem(elem, 'the price should be a number (can only contain numbers and a dot)');
+      return
+    }
+  }
+
+  if (charAmount > 1) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('maybe only one point');
+      return
+    }
+    drawPromptingElem(elem, 'maybe only one point');
+    return
+  }
+
+  if (charAmount == 1) {
+    var tempArr = tempVal.split('.');
+    if(tempArr[0].length < 1 || tempArr[1].length < 1) {
+      if ($(elem).next().next('.forvalJs-prompting')[0]) {
+        $($(elem).next().next('.forvalJs-prompting')[0]).text('price can not begin or end with a point');
+        return
+      }
+      drawPromptingElem(elem, 'price can not begin or end with a point');
+      return
+    }
+    if(!isNumeric(tempArr[0]) || !isNumeric(tempArr[1])) {
+      if ($(elem).next().next('.forvalJs-prompting')[0]) {
+        $($(elem).next().next('.forvalJs-prompting')[0]).text('the price should be a number (can only contain numbers and a dot)');
+        return
+      }
+      drawPromptingElem(elem, 'the price should be a number (can only contain numbers and a dot)');
+      return
+    }
+  }
+  removePromptingElem ($(elem).next().next('.forvalJs-prompting')[0])
+}
+
+function validatePassword(elem) {
+  var tempVal   = $(elem).val();
+  var hasNumber = hasSymbol = false;
+  var tempArr   = tempVal.split('');
+  for (var i = 0; i < tempArr.length; i++) {
+    if (isNumeric(tempArr[i])) {
+      hasNumber = true;
+    }
+    if (!isNumeric(tempArr[i])) {
+      hasSymbol = true;
+    }
+  }
+  if (!hasSymbol || !hasNumber) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('The password must contain at least one digit and at least one character');
+      return
+    }
+    drawPromptingElem(elem, 'password must contain at least one digit and at least one character');
+    return
+  }
+}
+
+function validateConfirmPassword(elem) {
+  var tempVal        = $(elem).val();
+  console.log("tempVal", tempVal);
+  var tempSiblingVal = $($(elem).siblings('.forvalJs-password')[0]).val();
+  console.log("tempSiblingVal", tempSiblingVal);
+  if (tempSiblingVal != tempVal) {
+    if ($(elem).next().next('.forvalJs-prompting')[0]) {
+      $($(elem).next().next('.forvalJs-prompting')[0]).text('passwords do not match');
+      return
+    }
+    drawPromptingElem(elem, 'passwords do not match');
+    return
+  }
 }
 
 function drawPromptingElem(elem, text) {
@@ -79,6 +234,7 @@ function drawPromptingElem(elem, text) {
                                                     'border-left'      : '3px solid red',
                                                     'box-sizing'       : 'border-box',
                                                     'padding-left'     : '6px',
+                                                    'user-select'      : 'none',
                                                     'display'          : 'block',
                                                     'transition'       : 'height .5s, margin-bottom .5s'
                                                   })
@@ -123,92 +279,17 @@ function calculateCharsInStr(str, char) {
   return count;
 }
 
-// function redrawPromptingElems() {
-//   var arrOfPromptingElems = $('.forvalJs-prompting');
-
-//   for (var i = 0; i < arrOfPromptingElems.length; i++) {
-
-//     var tempElTop  = getCoords(arrOfPromptingElems[i]).top;
-
-//     $(arrOfPromptingElems[i]).css({'top'  : tempElTop + 30 })
-//   }
-// }
-
-
-// $("input[id='form-email']").width();
-// console.log($("input[id='form-email']").outerWidth());
-
-// $(document).ready(function(){
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
 
 //   /* ↓↓↓ form validation ↓↓↓ */
 //   $('button[type="submit"]').click(function(e){
-
-//     if ($('#select-country').val() == 0) {
-//       e.preventDefault();
-//       $('#select-info').css({'transition':'height .5s','height':'30px'});
-//     }
-
-
-//     if ($('#input-password').val() == 0) {
-//       e.preventDefault();
-//       $('#input-password-info').text('Введите пароль!').css({'transition':'height .5s','height':'30px'});
-//     }
-//     if ($('#input-password').val() !== $('#input-confirm-pass').val()) {
-//       e.preventDefault();
-//       $('#input-password-info').text('Пароли не совпадают!').css({'transition':'height .5s','height':'30px'});
-//     }
-
-
-//     if ($('#input-country-code').val() == 0) {
-//       e.preventDefault();
-//       $('#input-phone-info').css({'transition':'height .5s','height':'30px'});
-//     }
-//     if ($('#input-company-code').val() == 0) {
-//       e.preventDefault();
-//       $('#input-phone-info').css({'transition':'height .5s','height':'30px'});
-//     }
-//     if ($('#input-company-code').val() == 0 || $('#input-phone').val().length < 7) {
-//       e.preventDefault();
-//       $('#input-phone').css({'transition':'height .5s','height':'30px'});
-//     }
-
 
 //     var temp = $('#input-email').val();
 //     if (temp.indexOf('@') <= 0 || temp.indexOf('@') > temp.lastIndexOf('.')) {
 //       e.preventDefault();
 //       $('#input-email-info').css({'transition':'height .5s','height':'30px'});
-//     }
-
-
-//     if ($('#input-firstName').val() == 0) {
-//       e.preventDefault();
-//       $('#input-firstName-info').css({'transition':'height .5s','height':'30px'});
-//     }
-
-//     if ($('#input-lastName').val() == 0) {
-//       e.preventDefault();
-//       $('#input-lastName-info').css({'transition':'height .5s','height':'30px'});
-//     }
-
-//   });
-
-
-
-//   $('#input-password').change(function(){
-//     if ($(this).val() === $('#input-confirm-pass').val()) {
-//       $('#input-password-info').css({'transition':'height .5s','height':'0px'});
-//     }
-//   });
-//   $('#input-confirm-pass').change(function(){
-//     if ($(this).val() === $('#input-password').val()) {
-//       $('#input-password-info').css({'transition':'height .5s','height':'0px'});
-//     }
-//   });
-
-
-//   $('#input-country-code, #input-company-code, #input-phone').change(function(){
-//     if ($('#input-country-code').val() != 0 && $('#input-company-code').val() != 0 && $('#input-phone').val() != 0 && ($('#input-phone').val().length >= 7)) {
-//      $('#input-phone-info').css({'transition':'height .5s','height':'0px'});
 //     }
 //   });
 
@@ -219,20 +300,3 @@ function calculateCharsInStr(str, char) {
 //       $('#input-email-info').css({'transition':'height .5s','height':'0px'});
 //     }
 //   });
-
-
-//   $('#input-firstName').change(function(e){
-//     if ($('#input-firstName').val() != 0 ) {
-//       $('#input-firstName-info').css({'transition':'height .5s','height':'0px'});
-//     }
-//   });
-
-
-//   $('#input-lastName').change(function(e){
-//     if ($('#input-lastName').val() != 0 ) {
-//       $('#input-lastName-info').css({'transition':'height .5s','height':'0px'});
-//     }
-//   });
-//   /* ↑↑↑ /form validation ↑↑↑ */
-
-// });
